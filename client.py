3b62e7b66938b20aa24707e5d7c83c97a6447961
cristian <cristi.mtr@gmail.com>
2014-11-16 20:19:49 +0100

    server and client can now handle JOIN requests

--- client.py
+++ client.py
@@ -2,16 +2,34 @@
 
 import socket
 
-
 SERVER_IP = '192.168.0.100'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((SERVER_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
-s.close()
+def get_my_ip():
+	# Hack. Need to develop function to discover own ip.
+	# so that the code can be deployed to machines
+	# and run immediately
+	return '192.168.0.101'
 
-print "received data:", data
+def join_workers(ip):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((SERVER_IP, TCP_PORT))
+	JOIN_REQUEST = 'JOIN,{}'.format(ip)
+	s.send(JOIN_REQUEST)
+	message = s.recv(BUFFER_SIZE)
+	s.close()
+	if message == '0':
+		return 0
+	return 1
+
+def main():
+	MY_IP = get_my_ip()
+	joined = join_workers(MY_IP)
+	if joined == 0:
+		print 'Joined successfully'
+	else:
+		print 'Something went wrong'
+	
+if __name__ == "__main__":
+	main()
--- server.py
+++ server.py
@@ -2,22 +2,41 @@
 
 import socket
 
-
 TCP_IP = '192.168.0.100'
 TCP_PORT = 5005
-BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
+BUFFER_SIZE = 1024  
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
+WORKERS = []
 
-conn, addr = s.accept()
-print 'Connection address:', addr
-
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print "received data:", data
-    conn.send(data)  # echo
+def handle_join(worker_ip):
+	if worker_ip not in WORKERS:
+		WORKERS.append(worker_ip)
+		print '\t\tNew worker added on ip {}'.format(worker_ip)
+		print '\t\t{}'.format(WORKERS)
+		return 0
+	print '\t\tThat ip is already in the worker list'
+	return 1
+		
+# the listen forever loop
+def main():
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind((TCP_IP, TCP_PORT))
 	
-conn.close()
+	while True:
+		s.listen(1)
+		conn, addr = s.accept()
+		data = conn.recv(BUFFER_SIZE)
+		if not data: 
+			conn.close()		
+		print "\tReceived data:", data
+		type = data.split(',')[0]
+		if type == 'JOIN':
+			if handle_join(data.split(',')[1]) == 0:
+				conn.send('0')
+			else:
+				conn.send('1')
+		conn.close()	
+
+if __name__ == '__main__':
+	main()
+	
