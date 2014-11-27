a921df8e42c454ef939c790bf4fa3b03fea48063
cristi.mtr <cristi.mtr@gmail.com>
2014-11-27 13:50:02 +0100

    MERGED LOCALHOST INTO MASTER NEW: 	workers now send both IP and PORT 	server now stores IPs and PORTs of active workers 	fixed problems with sockets not closing immediately on workers

--- client.py
+++ client.py
@@ -2,33 +2,66 @@
 
 import socket
 
-SERVER_IP = '192.168.0.100'
-TCP_PORT = 5005
+# SERVER
+SERVER_IP = 'localhost'
+SERVER_PORT = 5005
+# WORKER
+ip_port = ''
 BUFFER_SIZE = 1024
 
-def get_my_ip():
+def listen_for_tasks(port):
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect((SERVER_IP, TCP_PORT))
-	ip = s.getsockname()[0]
-	s.close()
-	return ip
-
-def join_workers(ip):
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	print port
+	s.bind(("",port))
+	while True:
+		s.listen(1)
+		conn, addr = s.accept()
+		data = conn.recv(BUFFER_SIZE)
+		if not data: 
+			conn.close()		
+		print "\tReceived data:", data
+		# TODO
+		conn.close()	
+	return 0
+	
+# notify SERVER of what port you are listening on
+def join_workers(ip_port):
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect((SERVER_IP, TCP_PORT))
-	JOIN_REQUEST = 'JOIN,{}'.format(ip)
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	s.connect((SERVER_IP, SERVER_PORT))
+	print '\tip_port inside join is {}'.format(ip_port)
+	# ex: 'JOIN,192.168.0.101,20000'
+	JOIN_REQUEST = 'JOIN,{}'.format(ip_port)
 	s.send(JOIN_REQUEST)
 	message = s.recv(BUFFER_SIZE)
+	print '\tmessage is {}'.format(message)
+	s.shutdown(socket.SHUT_RDWR) 
 	s.close()
 	if message == '0':
 		return 0
 	return 1
 
 def main():
-	MY_IP = get_my_ip()
-	joined = join_workers(MY_IP)
+	# bind socket and get port number
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	s.bind(("",0))
+	s.connect((SERVER_IP, SERVER_PORT))
+	ip_port = str(s.getsockname()[0]) + ',' + str(s.getsockname()[1])
+	print 'ip_port {} is {}'.format(type(ip_port),ip_port)
+	s.shutdown(socket.SHUT_RDWR) 
+	s.close()
+	#print 'port inside main in {}'.format(ip_port)
+	# check to see if you can join the pool of workers
+	joined = join_workers(ip_port)
 	if joined == 0:
 		print 'Joined successfully'
+		# listen for task
+		print 'ip_port is {}'.format(ip_port)
+		print type(ip_port)
+		listen_for_tasks(int(ip_port.split(',')[1]))
+	# else notify of something gone wrong		
 	else:
 		print 'Something went wrong'
 	
--- server.py
+++ server.py
@@ -2,24 +2,29 @@
 
 import socket
 
-TCP_IP = '192.168.0.100'
+TCP_IP = 'localhost'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024  
 
 WORKERS = []
 
-def handle_join(worker_ip):
-	if worker_ip not in WORKERS:
-		WORKERS.append(worker_ip)
-		print '\t\tNew worker added on ip {}'.format(worker_ip)
+def handle_join(worker_ip_port):
+	worker_ip_port = tuple(worker_ip_port)
+	if worker_ip_port not in WORKERS:
+		WORKERS.append(worker_ip_port)
+		print '\t\tNew worker added on port {}'.format(worker_ip_port)
 		print '\t\t{}'.format(WORKERS)
 		return 0
-	print '\t\tThat ip is already in the worker list'
+	print '\t\tThat port is already in the worker list'
 	return 1
 		
 # the listen forever loop
+# bind and listen
+# distribute requests by type
+# to their specific functions
 def main():
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	s.bind((TCP_IP, TCP_PORT))
 	
 	while True:
@@ -31,7 +36,8 @@ def main():
 		print "\tReceived data:", data
 		type = data.split(',')[0]
 		if type == 'JOIN':
-			if handle_join(data.split(',')[1]) == 0:
+			# expecting form "JOIN,<MY_PORT>"
+			if handle_join(data.split(',')[1:]) == 0:
 				conn.send('0')
 			else:
 				conn.send('1')
